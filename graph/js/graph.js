284774a8a3e1c45954f19a481e58e38ae05c151e
OilyPotato819 <o.schneider@share.epsb.ca>
2023-04-10 20:56:09 -0600

    Update graph.js

--- graph/js/graph.js
+++ graph/js/graph.js
@@ -28,149 +28,158 @@
 //    }
 // }
 
-const cnv = document.getElementById('canvas');
-const ctx = cnv.getContext('2d');
+// const cnv = document.getElementById('canvas');
+// const ctx = cnv.getContext('2d');
 
-cnv.width = window.innerWidth - 20;
-cnv.height = window.innerHeight - 60;
+// cnv.width = window.innerWidth - 20;
+// cnv.height = window.innerHeight - 60;
 
-let analyser, dataArray, bufferLength;
+// let analyser, dataArray, bufferLength;
 
-navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
-   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
+//    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-   let source = audioCtx.createMediaStreamSource(stream);
+//    let source = audioCtx.createMediaStreamSource(stream);
 
-   analyser = audioCtx.createAnalyser();
-   analyser.fftSize = 2048;
+//    analyser = audioCtx.createAnalyser();
+//    analyser.fftSize = 2048;
 
-   bufferLength = analyser.frequencyBinCount;
-   dataArray = new Uint8Array(bufferLength);
+//    bufferLength = analyser.frequencyBinCount;
+//    dataArray = new Uint8Array(bufferLength);
 
-   source.connect(analyser);
+//    source.connect(analyser);
 
-   draw();
-});
+//    draw();
+// });
 
-function draw() {
-   ctx.clearRect(0, 0, cnv.width, cnv.height);
+// function draw() {
+//    ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-   analyser.getByteFrequencyData(dataArray);
-   cnv.width = window.innerWidth - 20;
-   cnv.height = window.innerHeight - 60;
+//    analyser.getByteFrequencyData(dataArray);
+//    cnv.width = window.innerWidth - 20;
+//    cnv.height = window.innerHeight - 60;
 
-   drawCircle();
-   drawGraph();
+//    drawCircle();
+//    drawGraph();
 
-   requestAnimationFrame(draw);
-}
-
-function drawCircle() {
-   const circle = { x: cnv.width / 2, y: cnv.height / 2, r: 100, spikyness: 0.3 };
-   let angle = 0;
-   let start = {};
-   const pieceNum = 10;
-   const remove = 500;
-
-   let grd = ctx.createRadialGradient(circle.x, circle.y, circle.r - 20, circle.x, circle.y, circle.r + 50);
-   grd.addColorStop(1 / 7, 'red');
-   grd.addColorStop(2 / 7, 'orange');
-   grd.addColorStop(3 / 7, 'yellow');
-   grd.addColorStop(4 / 7, 'green');
-   grd.addColorStop(5 / 7, 'blue');
-   grd.addColorStop(6 / 7, 'indigo');
-   grd.addColorStop(7 / 7, 'violet');
-
-   ctx.fillStyle = grd;
-
-   ctx.beginPath();
-   for (let i = 0; i < pieceNum; i++) {
-      circlePiece(1 - (i % 2), i);
-   }
-   ctx.fill();
-
-   function circlePiece(clockwise, pieceI) {
-      let angleChange = (2 * Math.PI) / pieceNum / (bufferLength - remove);
-      let i, iStart, iExp, iAdd;
-
-      if (clockwise) {
-         iStart = 0;
-         iExp = function () {
-            return i < bufferLength - remove;
-         };
-         iAdd = 1;
-      } else {
-         iStart = bufferLength - remove;
-         iExp = function () {
-            return i > 0;
-         };
-         iAdd = -1;
-      }
-
-      for (i = iStart; iExp(); i += iAdd) {
-         dist = circle.r + dataArray[i] * circle.spikyness;
-
-         let x = circle.x + dist * Math.sin(angle);
-         let y = circle.y + dist * Math.cos(angle);
-
-         if (i == 1 && pieceI == 0) {
-            ctx.moveTo(x, y);
-            start.x = x;
-            start.y = y;
-         } else {
-            ctx.lineTo(x, y);
-         }
-
-         angle += angleChange;
-      }
-   }
-}
-
-function drawGraph() {
-   let pos = {};
-   let start = {};
-
-   ctx.beginPath();
-
-   for (let i = 0; i <= bufferLength; i++) {
-      pos.x = dataArray[i];
-
-      if (i == 0) {
-         pos.y = 0;
-
-         ctx.moveTo(0, cnv.height);
-         ctx.lineTo(0, 0);
-      }
-
-      ctx.lineTo(pos.x, pos.y);
-
-      pos.y += cnv.height / bufferLength;
-   }
-
-   ctx.fill();
-}
-
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
-// document.body.appendChild(renderer.domElement);
-
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-// camera.position.z = 5;
-
-// function animate() {
-//    requestAnimationFrame(animate);
-
-//    cube.rotation.x += 0.01;
-//    cube.rotation.y += 0.01;
-
-//    renderer.render(scene, camera);
+//    requestAnimationFrame(draw);
 // }
 
-// animate();
+// function drawCircle() {
+//    const circle = { x: cnv.width / 2, y: cnv.height / 2, r: 100, spikyness: 0.3 };
+//    let angle = 0;
+//    let start = {};
+//    const pieceNum = 10;
+//    const remove = 500;
+
+//    let grd = ctx.createRadialGradient(circle.x, circle.y, circle.r - 20, circle.x, circle.y, circle.r + 50);
+//    grd.addColorStop(1 / 7, 'red');
+//    grd.addColorStop(2 / 7, 'orange');
+//    grd.addColorStop(3 / 7, 'yellow');
+//    grd.addColorStop(4 / 7, 'green');
+//    grd.addColorStop(5 / 7, 'blue');
+//    grd.addColorStop(6 / 7, 'indigo');
+//    grd.addColorStop(7 / 7, 'violet');
+
+//    ctx.fillStyle = grd;
+
+//    ctx.beginPath();
+//    for (let i = 0; i < pieceNum; i++) {
+//       circlePiece(1 - (i % 2), i);
+//    }
+//    ctx.fill();
+
+//    function circlePiece(clockwise, pieceI) {
+//       let angleChange = (2 * Math.PI) / pieceNum / (bufferLength - remove);
+//       let i, iStart, iExp, iAdd;
+
+//       if (clockwise) {
+//          iStart = 0;
+//          iExp = function () {
+//             return i < bufferLength - remove;
+//          };
+//          iAdd = 1;
+//       } else {
+//          iStart = bufferLength - remove;
+//          iExp = function () {
+//             return i > 0;
+//          };
+//          iAdd = -1;
+//       }
+
+//       for (i = iStart; iExp(); i += iAdd) {
+//          dist = circle.r + dataArray[i] * circle.spikyness;
+
+//          let x = circle.x + dist * Math.sin(angle);
+//          let y = circle.y + dist * Math.cos(angle);
+
+//          if (i == 1 && pieceI == 0) {
+//             ctx.moveTo(x, y);
+//             start.x = x;
+//             start.y = y;
+//          } else {
+//             ctx.lineTo(x, y);
+//          }
+
+//          angle += angleChange;
+//       }
+//    }
+// }
+
+// function drawGraph() {
+//    let pos = {};
+//    let start = {};
+
+//    ctx.beginPath();
+
+//    for (let i = 0; i <= bufferLength; i++) {
+//       pos.x = dataArray[i];
+
+//       if (i == 0) {
+//          pos.y = 0;
+
+//          ctx.moveTo(0, cnv.height);
+//          ctx.lineTo(0, 0);
+//       }
+
+//       ctx.lineTo(pos.x, pos.y);
+
+//       pos.y += cnv.height / bufferLength;
+//    }
+
+//    ctx.fill();
+// }
+
+document.getElementById('canvas').hidden = true;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
+document.body.appendChild(renderer.domElement);
+
+let spike = 0;
+const geometry = new THREE.SphereGeometry(1);
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const sphere = new THREE.Mesh(geometry, material);
+
+scene.add(sphere);
+geometry.attributes.position.set([2], 10);
+sphere.geometry.attributes.position.needsUpdate = true;
+console.log(geometry.attributes.position);
+
+camera.position.z = 5;
+
+function animate() {
+   sphere.rotation.z += 0.01;
+
+   geometry.attributes.position.set([spike], 0);
+   spike += 1;
+
+   requestAnimationFrame(animate);
+
+   renderer.render(scene, camera);
+}
+
+animate();
