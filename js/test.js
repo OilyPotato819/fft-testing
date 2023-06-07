4bf0c89e88c362c9cd1251fcd177ac5b5e0f032d
OilyPotato8190 <o.schneider@share.epsb.ca>
2023-06-07 14:49:07 -0600

    Update test.js

--- js/test.js
+++ js/test.js
@@ -7,23 +7,75 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
 document.body.appendChild(renderer.domElement);
 
-let spike = 0;
-const geometry = new THREE.SphereGeometry(1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const sphere = new THREE.Mesh(geometry, material);
+// const geometry = new THREE.SphereGeometry(1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x0059ff, wireframe: true });
+// const sphere = new THREE.Mesh(geometry, material);
 
-scene.add(sphere);
+// scene.add(sphere);
 
-let positionAttribute = sphere.geometry.getAttribute('position');
-sphere.geometry.verticesNeedUpdate = true;
-sphere.geometry.attributes.position.array[0] = 10;
+// let position = sphere.geometry.getAttribute('position');
+// sphere.geometry.verticesNeedUpdate = true;
+// let normals = sphere.geometry.getAttribute('normal').array;
+// console.log(normals);
 
-camera.position.z = 10;
+// const sphere = new THREE.BufferGeometry();
+// const numPoints = 1000;
+// let vertices = new Float32Array(numPoints * 3);
+// for (let i = 0; i < numPoints; i++) {
+//   const pos = fibonacciSphere(numPoints, i);
+//   vertices[i * 3] = pos.x;
+//   vertices[i * 3 + 1] = pos.y;
+//   vertices[i * 3 + 2] = pos.z;
+// }
+// sphere.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+// const mesh = new THREE.Mesh(sphere, material);
+
+// const particlesMaterial = new THREE.PointsMaterial({ size: 0.02 });
+// const particles = new THREE.Points(sphere, particlesMaterial);
+
+// scene.add(particles);
+
+const radius = 1;
+const numPoints = 1000;
+
+const geometry = new THREE.SphereGeometry(radius, numPoints, numPoints);
+
+const vertices = geometry.getAttribute('position').array;
+for (let i = 0; i < vertices.length; i++) {
+  const { x, y, z } = fibonacciSphere(numPoints, i);
+  vertices[i * 3] = x;
+  vertices[i * 3 + 1] = y;
+  vertices[i * 3 + 2] = z;
+}
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+function fibonacciSphere(numPoints, point) {
+  const rnd = 1;
+  const offset = 2 / numPoints;
+  const increment = Math.PI * (3 - Math.sqrt(5));
+
+  const y = point * offset - 1 + offset / 2;
+  const r = Math.sqrt(1 - Math.pow(y, 2));
+
+  const phi = ((point + rnd) % numPoints) * increment;
+
+  const x = Math.cos(phi) * r;
+  const z = Math.sin(phi) * r;
+
+  return { x: x, y: y, z: z };
+}
 
 function animate() {
-  //   sphere.rotation.z += 0.01;
-  sphere.rotation.x += 0.01;
-  //   sphere.rotation.y += 0.01;
+  // particles.rotation.x += 0.01;
+  // particles.rotation.y += 0.01;
+  // particles.rotation.z += 0.005;
 
   //   positionAttribute.setXYZ(index + 1, x, y, z);
   //   positionAttribute.setXYZ(index + 2, x, y, z);
